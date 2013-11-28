bc7978f700dffe56324fe9355e4d1b2cb2e6a3d3
brandondove <brandon@pixeljar.net>
2013-11-28 02:08:13 -0800

    Removed the requirement for mongodb on the home page.
    Signed-off-by: brandondove <brandon@pixeljar.net>

--- routes/index.js
+++ routes/index.js
@@ -8,44 +8,9 @@ var mongo = require('mongodb').MongoClient;
 
 exports.index = function(req, res) {
 
-	mongo.connect('mongodb://127.0.0.1:27017', function(err, db) {
-
-		if (err) {
-
-			res.render('error', {
-				message: 'error connecting to database, is the database server running?'
-			});
-
-		} else {
-
-			//create the collection if it doesn't exist
-			//cap it at 10 records max... This will work like a queue
-			//first in, first out (so oldest record will be removed when cap is reached)
-			var collection = db.createCollection('testdb', {
-				'capped': true,
-				'size': 10,
-				'max': 10
-			}, function(err, col) {
-
-				//just insert a new element upon each request
-				col.insert({
-					'accessDate': new Date().toISOString()
-				}, function(err, docs) {
-					console.log(req);
-				});
-
-				//get all results, convert it to an array, then render the response.
-				col.find().toArray(function(err, results) {
-					res.render('index', {
-						title: '',
-						dynamiccontent: results
-					});
-				});
-
-			});
-
-		}
-
+	res.render('index', {
+		title: '',
+		dynamiccontent: results
 	});
 
 };
