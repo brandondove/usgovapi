3a7351d7f0549f3f645bd19c50f0f0b0641a8683
brandondove <brandon@pixeljar.net>
2013-11-28 01:26:35 -0800

    Petition Layout
    - Added breadcrumbs
    - Two column layout
    - Converted divs to tables
    - Bootstrappy goodness
    - Modified date format
    - Visual progress bar
    
    Signed-off-by: brandondove <brandon@pixeljar.net>

--- routes/petition.js
+++ routes/petition.js
@@ -25,9 +25,17 @@ exports.index = function(req, res) {
 					message: 'error connecting to WeThePeople API, is the service up? Are you behind a proxy?'
 				});
 			} else {
+				var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+				var created = new Date(petition.created*1000);
+				var deadline = new Date(petition.deadline*1000);
 				res.render('petition', {
+					title: "( Petition Detail )",
+					page_title: "Petition Detail",
 					petId: petId,
 					petition: petition,
+					petition_created: created.getDate() + ' ' + monthNames[created.getMonth()] + ' ' + created.getFullYear(),
+					petition_deadline: deadline.getDate() + ' ' + monthNames[deadline.getMonth()] + ' ' + deadline.getFullYear(),
+					progress_pct: (petition.signatureCount / petition.signaturesNeeded) * 100,
 					signatures: data.results
 				});
 			}
@@ -50,7 +58,7 @@ exports.index = function(req, res) {
 
 exports.signatureheatmap = function(req, res) {
 	res.render('signatureheatmap', {
-		title: 'Signature Heat Map',
+		title: 'Petitions: Signature Heat Map',
 		petitionId: req.params.id
 	});
 }
